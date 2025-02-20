8101ed06c105656c07c4136ff33683ad820dbc73
MB <Martin.Bosticky@cleancoqld.com.au>
2025-02-21 06:17:36 +1000

    configure for github deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { ghPages } from "vite-plugin-gh-pages";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: "/flashcards-edtech/",
+  plugins: [react()],
+  base: "/Flashcard/",
 });
